a78ff7a1647ffcd288857fb1c1f218086c8818d1
Biaou ahmed <ahmed.alidjinou@hotmail.com>
2020-12-02 15:24:04 +0100

    add of number to letter function

--- exercice.js
+++ exercice.js
@@ -1,12 +1,18 @@
 function exo1() {
-    alert("hello");
+    alert(isEven(8));
 }
+
 function isEven(nb) {
     var result = false;
     if (nb % 2 === 0) {
         result = true;
     }
     return result;
+}
+
+function isEven2(nb) {
+
+    return (nb % 2 === 0);
 }
 
 function firstWorld(value) {
@@ -16,7 +22,19 @@ function firstWorld(value) {
 
 }
 
-function firstwordupper(value) {
+function exo2() {
+    let p = prompt("Phrase ?");
+    alert(firstWorld(p));
+}
+
+function firstWorld2(value) {
+
+
+    return value.split(' ')[0];
+
+}
+
+function firstWordUpper(value) {
     var inter = value.split(' ');
 
     var result = inter[0].toUpperCase();
@@ -27,6 +45,11 @@ function firstwordupper(value) {
 
 }
 
+function exo3() {
+    let p = prompt("Phrase ?");
+    alert(firstWordUpper(p));
+}
+
 function firstleter(value) {
     var result = value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
 
@@ -34,58 +57,70 @@ function firstleter(value) {
 
 }
 
-function camelcase(value) {
+function exo4() {
+    let p = prompt("Phrase ?");
+    alert(firstleter(p));
+}
+
+function camelCase(value) {
     var inter = value.split(' ');
-    var result = inter[0];
+    var result = inter[0].toLowerCase();
     for (var i = 1; i < inter.length; i++) {
-        result += inter[i].charAt(0).toUpperCase() + inter[i].slice(1)
+        result += firstleter(inter[i]);
     }
 
     return result;
 
 }
 
+function exo5() {
+    let p = prompt("Phrase ?");
+    alert(camelCase(p));
+}
 
 function parcourirTableau() {
     var array = [1, 2, 3, 4, 5, 4, 3, 2, 1];
 
-    for (var i = 0; i < array.length; i++) {
-        let txt = alert(array[i]);
+    for (let i = 0; i < array.length; i++) {
+        alert(array[i]);
     }
 
+}
+
+function exo6() {
+    parcourirTableau();
 }
 
 function force(value) {
     var n = 0;
-    var regex = "/\d/g";
-    var reg = "/[a-z]/";
+    var regex = /\d/g;
+    var regexLower = /[a-z]/;
+    var regexUpper = /[A-Z]+/;
+    var regexCharSpe = /[!#%_?*$\\]/;
 
-    if (regex.match(value)) {
+    if (value.match(regex)) {
         n += 10;
     }
-    if (reg.match(value)) {
+    if (value.match(regexLower)) {
         n += 26;
     }
-    return Math.round(value.length * Math.log(n) / Math.log(2));
+    if (value.match(regexUpper)) {
+        n += 26;
+    }
+    if (value.match(regexCharSpe)) {
+        n += 8;
+    }
+    console.log(n);
+    return Math.round(value.length * Math.log2(n));
 }
 
-function exo9(text) {
-
-    var n = 0
-    if (text.match(/[0-9]+/)) {
-        n += 10
-    }
-    if (text.match(/[a-z]+/)) {
-        n += 26
-    }
-    if (text.match(/[A-Z]+/)) {
-        n += 26
-    }
-    var strength = Math.round(text.length * Math.log(n) / Math.log(2))
-    return strength;
+function exo7() {
+    var text = prompt("Quel est votre mot de passe");
+    alert(force(text));
 }
-function exo6() {
-    var text = prompt("Quel est le genre de votre tamagotchi ?");
+
+function tamagoGender(text) {
+
 
     switch (text.toLowerCase()) {
 
@@ -104,18 +139,27 @@ function exo6() {
 
 }
 
-function exo7() {
+function exo8() {
+    var text = prompt("Quel est le genre de votre tamagotchi ?");
+    tamagoGender(text);
+}
+
+function strengthPassword() {
     var text = prompt("un mot de passe");
 
     if (exo9(text) > 104) {
         alert("bravo");
     } else {
-        exo7();
+        strengthPassword();
     }
 
 }
 
-function month(mois) {
+function exo9() {
+    strengthPassword()
+}
+
+/* function month(mois) {
     switch (mois) {
 
         case 1:
@@ -158,9 +202,25 @@ function month(mois) {
         default:
             exo6();
     }
+} */
+
+function month(monthNb) {
+    var m;
+    var array = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "novembre", "decembre"]
+    if (monthNb > array.length || monthNb < 1) {
+        console.log('erreur');
+    } else {
+        m = array[monthNb - 1]
+    }
+    return m;
 }
 
-function aujourdhui(){
+function exo10() {
+    var nb = prompt("le mois en nombre");
+    month(nb);
+}
+
+function aujourdhui() {
     let date1 = new Date();
 
     let dateLocale = date1.toLocaleString('fr-FR', {
@@ -168,18 +228,97 @@ function aujourdhui(){
         year: 'numeric',
         month: 'long',
         day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric'
+        /*   hour: 'numeric',
+          minute: 'numeric',
+          second: 'numeric' */
     });
 
     alert(dateLocale)
 }
-function numberToLetters(){
-    
+
+function numberToLetters() {
+    var number = prompt("entrez un nombre");
+    var result = "";
+    var nbinf = ["zero", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix", "onze", "douze", "treize", "quatorze", "quinze", "seize"]
+
+
+
+    do {
+        console.log(result);
+        console.log(number + ' ' + String(number).length);
+        if (number.length == 4) {
+            var indexd = number.split('')[0];
+            console.log(indexd);
+            if (indexd == '1') {
+                result += 'mille';
+                number = number % 1000;
+            } else {
+                result += ' ' + nbinf[indexd] + ' mille';
+                number = number % (100 * parseInt(indexd));
+            }
+            console.log(result);
+        } else if (String(number).length == 3) {
+            var indexd = String(number).split('')[0];
+            console.log(indexd);
+            if (indexd == '1') {
+                result += 'cent';
+                number = number % 100;
+            } else {
+                result += ' ' + nbinf[indexd] + ' cent';
+                console.log((100 * parseInt(indexd)));
+                number = number % (100 * parseInt(indexd));
+            }
+            console.log(result);
+        } else if (number <= 10) {
+
+            if (result == null) {
+                result += nbinf[number];
+            } else {
+                if (number == 1) {
+                    result += " et " + nbinf[number];
+                } else {
+                    result += "-" + nbinf[number];
+                }
+
+
+            }
+
+            number -= number
+        } else if (number <= 16) {
+            console.log("test15")
+            result += " " + nbinf[number];
+            number -= number
+        } else if (number <= 19) {
+            result += " dix";
+            number = number % 10;
+        } else if (number < 30) {
+            result += " vingt";
+            number = number % 20;
+        } else if (number < 40) {
+            result += " trente";
+            number = number % 30;
+        } else if (number < 50) {
+            result += " quarante";
+            number = number % 40;
+        } else if (number < 60) {
+            result += " cinquante";
+            number = number % 50;
+        } else if (number < 80) {
+            console.log("in 60");
+            result += " soixante";
+            number = number % 60;
+            console.log(number);
+        } else if (number < 100) {
+            result += " quatre-vingts";
+            number = number % 80;
+        }
+    } while (number > 0);
+
+
+    alert(result);
 }
-month(3);
-aujourdhui();
+//month(3);
+//aujourdhui();
 //console.log(force("123"));
 /*
 parcourirTableau();
